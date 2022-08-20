2b0370c2860b84ca5b14c8a5c81f32b4b787322e
mertkamir <m.ozankamir@gmail.com>
2022-08-20 21:19:51 +0300

    inheritance

---
+++ java/inheritance/src/CustomerManager.java
@@ -0,0 +1,10 @@
+public class CustomerManager extends PersonManager{
+
+    /*public void List (){
+        System.out.println("Listed");
+    }
+
+    public void Add (){
+        System.out.println("Added");
+    }*/
+}
---
+++ java/inheritance/src/EmployeeManager.java
@@ -0,0 +1,14 @@
+public class EmployeeManager extends PersonManager {
+
+    /*public void List (){
+        System.out.println("Listed");
+    }
+
+    public void Add (){
+        System.out.println("Added");
+    }*/
+
+    public void BestEmployee() {
+        System.out.println("Best employee of month");
+    }
+}
